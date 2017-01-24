7a9fc89ffe14d73cdaccea992646d81c0fe9bece
saddamalmahali <saddamalmahali@yahoo.co.id>
2017-01-24 19:50:40 +0700

    tambah dashboard admin dan ketika register langsung login

--- resources/views/alumni/tracer/done.blade.php
+++ resources/views/alumni/tracer/done.blade.php
@@ -1,7 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
-        div.col-
-    </div>
+    <section class="content-header">
+        <h1> 
+            ADMIN
+            <p><small>Tracer Index</small></p>
+            
+        </h1>
+        
+    </section>
+    <section class="content">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="box box-primary">
+                    <div class="box-header with-border">
+                        <h2 class="box-title"><center>TERIMA KASIH ATAS PARTISIPASINYA</center></h2>
+                    </div>
+                    <div class="panel-body">
+                        <h3>User Yang Anda Gunakan Telah Mengisi Quesioner didalam sistem.</3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
